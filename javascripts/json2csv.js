b9c2cc0a9c82912e13e6c04a61e6f57f2125c14e
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-11-30 20:44:39 -0500

    clean up csv parsing

--- javascripts/json2csv.js
+++ javascripts/json2csv.js
@@ -1,23 +1,22 @@
 var csvConverter = {
 	ConvertToCSV: function (objArray) {
-	  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+	  var array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
 	  var str = '';
 	  var headers = Object.keys(array[0]);
+
 	  headers.forEach(function(item) {
-	    str += item;
-	    str += ',';
+	    str += item + ',';
 	  });
 	  str += '\r\n';
-	  for (var i = 0; i < array.length; i++) {
-	    var line = '';
-	    for (var index in array[i]) {
-	      if (line != '') line += ','
-	 
-	        line += array[i][index];
-	      }
-	 
-	      str += line + '\r\n';
-	    }
+
+	  array.forEach(function (item, index) {
+	  	var line = '';
+	  	for (var index in item) {
+	  		if (line !== '') { line += ','; }
+	  		line += item[index];
+	  	}
+	  	str += line + '\r\n';
+	  });
 	 return str;
 	}
-}
+};
